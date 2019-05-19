c6c8369ef1726532100917a81a56e0ca46b98f1f
Hélio Márcio Filho <heliommsfilho@tutanota.com>
2019-05-19 15:20:00 +0100

    2.4. Criando a camada de persistência do projeto

---
+++ src/main/java/com/heliommsfilho/erp/repository/RamoAtividades.java
@@ -0,0 +1,40 @@
+package com.heliommsfilho.erp.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import com.heliommsfilho.erp.model.RamoAtividade;
+
+public class RamoAtividades implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private EntityManager manager;
+
+	public RamoAtividades() {
+
+	}
+
+	public RamoAtividades(EntityManager manager) {
+		this.manager = manager;
+	}
+	
+	public List<RamoAtividade> pesquisar(String descricao) {
+		CriteriaBuilder cb = manager.getCriteriaBuilder();
+		CriteriaQuery<RamoAtividade> c = cb.createQuery(RamoAtividade.class);
+		Root<RamoAtividade> root = c.from(RamoAtividade.class);
+		
+		c.select(root);
+		c.where(cb.like(root.get("descricao"), descricao + "%"));
+		
+		TypedQuery<RamoAtividade> query = manager.createQuery(c);
+		
+		return query.getResultList();
+	}
+}
